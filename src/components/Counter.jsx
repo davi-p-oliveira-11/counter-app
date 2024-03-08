048acdfbd01540daa85cb5aadb15669ea6a4d99c
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-03-07 22:34:43 -0300

    added the entire logic

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,15 +2,31 @@ import { useState } from "react"
 
 const Counter = () => {
   
-  const [count, setCount] = useState(0);
+  let [count, setCount] = useState(0);
+
+  function handleIncrement() {
+    setCount(count + 1)
+  }
+
+  function handleDecrement() {
+    setCount(count - 1)
+  }
+
+  function resetCount() {
+    setCount(count = 0)
+  }
+   
    
   return (
     <div>
       <div>
         <p>{count}</p>
         <div>
-          <button>Increment</button>
-          <button>Increment</button>
+          <button onClick={resetCount}>Reset</button>
+        </div>
+        <div>
+          <button onClick={handleIncrement}>+</button>
+          <button onClick={handleDecrement}>-</button>
         </div>
       </div>
     </div>
