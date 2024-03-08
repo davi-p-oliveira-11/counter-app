819c4ca3fe3577ae83ea916459c9ac3e3746fef8
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-03-07 22:08:41 -0300

    unistalled styled components

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,6 +1,19 @@
+import { useState } from "react"
+
 const Counter = () => {
+  
+  const [count, setCount] = useState(0);
+   
   return (
-    <div>Counter</div>
+    <div>
+      <div>
+        <p>{count}</p>
+        <div>
+          <button>Increment</button>
+          <button>Increment</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
