3297250bbec53cf0f388810e85dd75c5d1a8045a
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-03-08 00:15:36 -0300

    redone file structure for plain css

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Counter from "./components/Counter"
 function App() {
 
   return (
-    <div className="bg">
+    <div className="bg-black">
      <Counter />
     </div>
   )
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,36 +1,36 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const Counter = () => {
-  
   let [count, setCount] = useState(0);
 
   function handleIncrement() {
-    setCount(count + 1)
+    setCount(count + 1);
   }
 
   function handleDecrement() {
-    setCount(count - 1)
+    setCount(count - 1);
   }
 
   function resetCount() {
-    setCount(count = 0)
+    setCount((count = 0));
   }
-   
-   
+
   return (
-    <div className="bg-slate-500">
+    <div className="">
       <div>
-        <h1 className="bg-blue-500 text-white p-4">{count}</h1>
+        <h1 className="">{count}</h1>
         <div>
           <button onClick={resetCount}>Reset</button>
         </div>
         <div>
-          <button className="btn bg-black " onClick={handleIncrement}>+</button>
+          <button className="" onClick={handleIncrement}>
+            +
+          </button>
           <button onClick={handleDecrement}>-</button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Counter
+export default Counter;
